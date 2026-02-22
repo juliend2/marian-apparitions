c309a0b9f3c59935c84a87b424199e0faa75320e
Julien Desrosiers <juliend2@gmail.com>
2026-02-21 19:29:22 -0500

    optional 'by' in approval statement

--- viewmodel/event_view_model.go
+++ viewmodel/event_view_model.go
@@ -19,14 +19,8 @@ func NewEventVM(event *model.Event) *EventViewModel {
 	return &EventViewModel{*event}
 }
 
-// Returns whether the church ("Catholic Church" and so on) approved the event
-func (vm *EventViewModel) IsChurchApproved(churchNameSubstr string) bool {
-	for _, block := range vm.Event.Blocks {
-		if strings.Contains(block.ChurchAuthority, churchNameSubstr) && block.AuthorityPosition == "approved" {
-			return true
-		}
-	}
-	return false
+func (vm *EventViewModel) HasAnyApproval() bool {
+	return vm.GetApproverChurch("Catholic") != "" || vm.GetApproverChurch("Orthodox") != "" || vm.GetApproverChurch("Anglican") != ""
 }
 
 func (vm *EventViewModel) GetApproverChurch(churchNameSubstr string) string {
